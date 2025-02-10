ddaa6fb68ce0a62de57adbe21e37ec49f5d9243d
Tanveer Singh Jandu <Tjandu4495@conestogac.on.ca>
2025-02-09 23:22:37 -0500

    Fixed missing subtract() function and updated main() to call it.

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
-
-void main() {
+int main() {
 
 	printWelcomeMenu();
 
@@ -14,15 +15,22 @@ void main() {
 	int inputNum;
 
 	printf("Enter operation number: ");
-	scanf_s("%1o", &inputNum);
+	scanf("%d", &inputNum);
 
 	switch (inputNum)
 	{
 	case 1:
 		add();
+		break;
+
+	case 2:
+		subtract();
+		break;
 	}
 
+	return 0;
 }
+
 
 void printWelcomeMenu() {
 	printf(" **********************\n");
@@ -39,9 +47,19 @@ void printOptions() {
 void add() {
 	double num1, num2, result;
 	printf("Enter the first value:");
-	scanf_s("%lf", &num1);
+	scanf("%lf", &num1);
 	printf("Enter the second value:");
-	scanf_s("%lf", &num2);
+	scanf("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+	double num1, num2, result;
+	printf("Enter the first value: ");
+	scanf("%lf", &num1);
+	printf("Enter the second value: ");
+	scanf("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
